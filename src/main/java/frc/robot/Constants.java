9b99f070b6ef7e8c18cb8fb75c260e130d6fcec6
Nick Avtges <navtges@gmail.com>
2020-01-29 08:15:23 -0500

    Making constants cool

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,15 +19,15 @@ package frc.robot;
  */
 public final class Constants {
 
-    //CPM constants
-    public static final int CPM_MOTOR = 00;
-    public static final int CPM_JOINT_ENCODER_CHANNEL_1A = 01;
-    public static final int CPM_JOINT_ENCODER_CHANNEL_1B = 02;
-    public static final int CPM_PNEUMATICS_FORWARD = 03;
-    public static final int CPM_PNEUMATICS_REVERSE = 04;
-    public static final int CPM_DIGITAL_INPUT_CHANNEL = 00;
-    public static final int CPM_WRIST_ENCODER_COUNTS_PER_REV = 06;
-    public static final float CPM_PHASE_2_WHEEL_ROTATIONS = 4;
+	// CPM constants
+	public static final int CPM_MOTOR = 00;
+	public static final int CPM_JOINT_ENCODER_CHANNEL_1A = 01;
+	public static final int CPM_JOINT_ENCODER_CHANNEL_1B = 02;
+	public static final int CPM_PNEUMATICS_FORWARD = 03;
+	public static final int CPM_PNEUMATICS_REVERSE = 04;
+	public static final int CPM_DIGITAL_INPUT_CHANNEL = 00;
+	public static final int CPM_WRIST_ENCODER_COUNTS_PER_REV = 06;
+	public static final float CPM_PHASE_2_WHEEL_ROTATIONS = 4;
 
 	// Shooter constants
 	public static final int SHOOTER_MOTOR = 07;
@@ -42,14 +42,13 @@ public final class Constants {
 	public static final int INTAKE_DEPLOY_PISTON = 0;
 	public static final int INTAKE_RETRACT_PISTON = 7;
 
-	
-    /********************************************************************
+	/********************************************************************
     * 
-    *     ____       _          
-    *    / __ \_____(_)   _____ 
-    *   / / / / ___/ / | / / _ \
-    *  / /_/ / /  / /| |/ /  __/
-    * /_____/_/  /_/ |___/\___/ 
+    *                ____       _          
+    *       --------/ __ \_____(_)   _____ 
+    *      --------/ / / / ___/ / | / / _ \
+    *     --------/ /_/ / /  / /| |/ /  __/
+    *    --------/_____/_/  /_/ |___/\___/ 
     * 
 	*/
 	
